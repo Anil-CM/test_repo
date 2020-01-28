72d07246e90b000e0cf86536247fd7b077e23aea
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2020-01-28 14:16:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,20 @@ locals {
 }
 
 data "ibm_resource_group" "resource_group" {
-  name = var.resource_group
+  name = "${var.resource_group}"
 }
 
 resource "ibm_container_vpc_cluster" "cluster" {
   name              = "us-test-2"
   vpc_id            = "71ac0bbb-008e-46b1-96d3-66741c163a46"
-  flavor            = var.flavor
-  worker_count      = var.worker_count
+  flavor            = "${var.flavor}"
+  worker_count      = "${var.worker_count}"
   disable_public_service_endpoint = true
-  resource_group_id = data.ibm_resource_group.resource_group.id
+  resource_group_id = "${data.ibm_resource_group.resource_group.id}"
 
   zones {
-    name      = local.ZONE
+    name      = "us-south-1"
     subnet_id = "bd58de33-7639-43f8-914b-524042b3ab50"
   }
   
 }
-
-data "ibm_container_cluster_config" "cluster_config" {
-  cluster_name_id = ibm_container_vpc_cluster.cluster.id
-}
-
